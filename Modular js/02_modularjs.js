bee10baa08b1c703e1152e82ba2cb23a83e10fe5
nashn3r1 <nashn3r1@gmail.com>
2017-07-11 23:16:17 +0530

    changs

--- Modular js/02_modularjs.js
+++ Modular js/02_modularjs.js
@@ -1,54 +1,60 @@
-function emp(empid,ename,empsal,deptno)
-{
-	this.empid = empid;
+function Employee(eid,ename,esal){
+	this.eid = eid;
 	this.ename = ename;
-	this.empsal = empsal;
-	this.deptno = deptnol;
-	
+	this.esal = esal;
 }
-
-
-var  EmployeeList = (function() {
-     var  Employee = [];
-// exports
-     return
-	 {
-     addemp: function(emp) { Employee.push(emp);},
-     getemp: function() { return Employee; },
-	 sort : function() {
+function EmployeeList(ary){
+	
+	//this.ary  = ary;
+	console.log(ary.length);
+		return {
+					arrSort : function() {
+					/*	var i=0,j=0,len = ary.length;
+						console.log(len);
+						var temp = new Employee();
+						for(i=0 ; i<len ; i++){
+							for(j=i+1 ; j<len ; j++){
+								if(ary[j].eid < ary[i].eid){
+									temp = ary[j];
+									ary[j] = ary[i];
+									ary[i] = temp;
+								}
+							}
+						}*///both the ways for sorting are working
+						ary.sort(function(a, b){return a.eid - b.eid});
+						
+						
+					},
+					
+					display : function() {
+						
+						console.log(ary);
+					}
 			
-			Employee.sort(function(a,b){ return a.empid - b.empid};
-		 
-	 }
-     }
-})();
-
-
-
-function test()
-{
-	var emp1 = new emp(10,"messi",400,1);
-	var emp2 = new emp(11,"neymar",300,1);
-	var emp3 = new emp(9,"suarez",300,1);
-	var emp4 = new emp(4,"rakitic",100,1);
+			
+		}
 	
-	EmployeeList.addemp(emp1);
-	EmployeeList.addemp(emp2);
-	EmployeeList.addemp(emp3);
-	EmployeeList.addemp(emp4);
 	
-	EmployeeList.sort();
-    var arr = EmployeeList.getemp();
-	var len = arr.length;
-	for ( var i = 0;i<len; i++)
-	{
-		document.write("empid :" + arr[i].empid);
-		document.write("ename:" + arr[i].ename);
-		document.write("empsal :" + arr[i].empsal);
-		document.write("deptno :" + arr[i].deptno);
+		
+	    
+
+		//document.getElementById("div1").innerHTML = "The Sorted employee list is "+ary;
 	}
-}	
-	
-	
-	
-	
+
+
+function test(){
+	var emp = new Array();
+		emp[0] = new Employee(3,"Rajesh",10030);
+		emp[1] = new Employee(31,"Rajesh",10200);
+		emp[2] = new Employee(2,"Rajesh",10090);
+		emp[3] = new Employee(23,"Rajesh",10100);
+		emp[4] = new Employee(12,"Rajesh",10400);
+		emp[5] = new Employee(99,"Rajesh",10500);
+		emp[6] = new Employee(14,"Rajesh",10400);
+		emp[7] = new Employee(36,"Rajesh",10600);
+		emp[8] = new Employee(32,"Rajesh",10400);
+		emp[9] = new Employee(59,"Rajesh",10100);
+	var empList = new EmployeeList(emp);
+	empList.arrSort();
+	empList.display();
+}
